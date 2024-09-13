ba500ba2089b6fdd1d90455ed71107112de6b6b1
Greg Pedersen <gregpedersen99@gmail.com>
2024-09-13 12:21:32 -0700

    Adding buttons to functionality, hopefully these will get cleaned up as I get further into this project

--- flaskProject/main.py
+++ flaskProject/main.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Welcome to the website of Greg Pedersen"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    output = ''
+    if request.method == 'POST':
+        if request.form['action'] == 'generate_text_1':
+            output = 'Hello Gregory!'
+        elif request.form['action'] == 'generate_text_2':
+            output = 'You are not Gregory, go away!'
+    return render_template('login.html', output=output)
 
-if __name__ == "__main__":
-    # Get the PORT environment variable or default to 5000
+if __name__ == '__main__':
+    # Get the port from the environment variable, default to 5000 if not set
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # Run the application on all network interfaces at the specified port
+    app.run(host='0.0.0.0', port=port, debug=True)
