2d8b031bb1b9b446368205b20c31c15987180f13
Greg Pedersen <47991971+gregpedersen99@users.noreply.github.com>
2024-09-15 14:35:39 -0700

    Update main.py
    Incorporating new username and password input

--- flaskProject/main.py
+++ flaskProject/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import os
 
 app = Flask(__name__)
@@ -8,10 +8,17 @@ def index():
     output = ''
     if request.method == 'POST':
         if request.form['action'] == 'generate_text_1':
-            output = 'Hello Gregory!'
+            return redirect(url_for('login_page'))
         elif request.form['action'] == 'generate_text_2':
             output = 'You are not Gregory, go away!'
     return render_template('login.html', output=output)
+
+@app.route('/login_page')
+def login_page():
+    return render_template('login2.html')
+
+
+
 
 if __name__ == '__main__':
     # Get the port from the environment variable, default to 5000 if not set
@@ -24,5 +31,4 @@ if __name__ == '__main__':
   #Tod do List:
     #Format and add a good background photo
     #Change button formats 
-    #Add password and username option for a Gregory Pederen = 'Yes' submission 
-    
+    #Add password and username option for a Gregory Pederen = 'Yes' ubmission 
